d298a6cda26cf7a73d4caae48af69a9d7ca85807
Jono <160443350+JonoBeach@users.noreply.github.com>
2026-04-25 16:57:53 +1200

    Added fake code to index.ts
    Added fake implementation for agentAuthenticate() and checkAgentPermission()

--- src/agent/index.ts
+++ src/agent/index.ts
@@ -1,15 +1,23 @@
 import type { Agent, Permission } from "../core/types.js";
+import {fakeBlockchain} from "../core/blockchain.js";
 
 /**
  * Authenticate an AI agent against the blockchain.
  */
-export function agentAuthenticate(agent: Agent): void {
-  console.log("agentAuthenticate", { agent });
+export function agentAuthenticate(agent: Agent): boolean {
+  if (agent == fakeBlockchain.agent) {
+    return true;
+  } return false;
+  //console.log("agentAuthenticate", { agent });
 }
 
 /**
  * Check if the authenticated AI agent has a given permission.
  */
-export function checkAgentPermission(agent: Agent, perm: Permission): void {
-  console.log("checkAgentPermission", { agent, perm });
+export function checkAgentPermission(agent: Agent, perm: Permission): boolean {
+  if (perm in fakeBlockchain.permissions && agent == fakeBlockchain.agent) {
+    return true;
+  } return false;
+
+  //console.log("checkAgentPermission", { agent, perm });
 }
