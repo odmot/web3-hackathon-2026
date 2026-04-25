6cc60ea41e219f1e5046d8990d8a0421a0f1fead
Jono <160443350+JonoBeach@users.noreply.github.com>
2026-04-25 17:16:31 +1200

    Updated fakeBlockChain functions

--- src/agent/index.ts
+++ src/agent/index.ts
@@ -5,19 +5,20 @@ import {fakeBlockchain} from "../core/blockchain.js";
  * Authenticate an AI agent against the blockchain.
  */
 export function agentAuthenticate(agent: Agent): boolean {
-  if (agent == fakeBlockchain.agent) {
-    return true;
+  for (var entry of fakeBlockchain) {
+    if (entry.agent == agent) {
+      return true;
+    }
   } return false;
-  //console.log("agentAuthenticate", { agent });
 }
 
 /**
  * Check if the authenticated AI agent has a given permission.
  */
 export function checkAgentPermission(agent: Agent, perm: Permission): boolean {
-  if (perm in fakeBlockchain.permissions && agent == fakeBlockchain.agent) {
-    return true;
+  for (var entry of fakeBlockchain) {
+    if (perm in entry.permissions && entry.agent == agent) {
+      return true;
+    }
   } return false;
-
-  //console.log("checkAgentPermission", { agent, perm });
-}
+} 
