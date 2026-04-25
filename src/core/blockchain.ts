bd15424664d93c1e831572fa936042e940e4602d
odmot <139671314+odmot@users.noreply.github.com>
2026-04-25 17:00:38 +1200

    make fake blockchain an array

--- src/core/blockchain.ts
+++ src/core/blockchain.ts
@@ -1,7 +1,14 @@
 import type { Agent, Permission, Owner } from "../core/types.js";
 
-export const fakeBlockchain: { owner: Owner, agent: Agent, permissions: Permission[] } = {
-  owner: "test owner",
-  agent: "test agent",
-  permissions: ["test perm1", "test perm2"]
+interface FakeBlockchain {
+  owner: Owner,
+  agent: Agent,
+  permissions: Permission[]
 }
+
+export const fakeBlockchain: FakeBlockchain[] = [
+  { owner: "test owner1", agent: "test agent1", permissions: ["test perm1", "test perm2"] },
+  { owner: "test owner2", agent: "test agent2", permissions: ["test perm2", "test perm3"] },
+  { owner: "test owner3", agent: "test agent3", permissions: ["test perm3", "test perm4"] },
+  { owner: "test owner4", agent: "test agent4", permissions: ["test perm4", "test perm5"] }
+]
