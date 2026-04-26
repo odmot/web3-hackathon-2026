7ebfe790191db11b179e7f35a1290e797f065544
stan779-UOA <265966824+stan779-UOA@users.noreply.github.com>
2026-04-26 12:10:28 +1200

    Added additional test agents to blockchain

--- src/core/blockchain.ts
+++ src/core/blockchain.ts
@@ -10,5 +10,7 @@ export const fakeBlockchain: FakeBlockchain[] = [
   { owner: "test owner1", agent: "test agent1", permissions: ["test perm1", "test perm2"] },
   { owner: "test owner2", agent: "test agent2", permissions: ["test perm2", "test perm3"] },
   { owner: "test owner3", agent: "test agent3", permissions: ["test perm3", "test perm4"] },
-  { owner: "test owner4", agent: "test agent4", permissions: ["test perm4", "test perm5"] }
+  { owner: "test owner4", agent: "test agent4", permissions: ["test perm4", "test perm5"] },
+  { owner: "Shun", agent: "AgentGPT", permissions: ["Allow Purchases"] },
+  { owner: "Josh", agent: "ClaudeAgent", permissions: ["Allow Purchases", "Allow Purchase Past Limit"] }
 ]
